261d3e356d4ca56d2ba2826481948212e6bb90a3
charmingk7 <skylightcar@gmail.com>
2021-01-31 12:15:51 +0900

    게시판 프로젝트

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,7 +19,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다.
 //여기서는 SpringRunner라는 스프링 실행자를 사용
 //즉, 스프링 부트 테스트와 JUnit 사이에 연결자 역할
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    }
+)
 //여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션
 //선언할 경우 @Controller, @ControllerAdvice 등을 사용할 수 있다.
 //단, @Service, @Component, @Repository 등은 사용할 수 없다.
@@ -27,6 +35,7 @@ public class HelloControllerTest {
     //웹 API를 테스트할 때 사용
     //스프링 MVC 테스트의 시작점이다.
     //이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있다.
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -44,6 +53,7 @@ public class HelloControllerTest {
         //Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증한다.
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
