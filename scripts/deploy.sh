4807391a86385ef113a18eb7a6990be3f7d7bc6f
charmingk7 <skylightcar@gmail.com>
2021-02-03 17:17:05 +0900

    배포 자동화 구성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springproject1 | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션pid: $CURRENT_PID"
 
